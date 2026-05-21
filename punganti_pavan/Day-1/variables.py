e12689a2ac3d819ac038f4e63c2a11b32349278b
punagantipavan4 <pavan.klr234@gmail.com>
2026-05-21 23:53:58 +0530

    day1 programming basics

---
+++ punganti_pavan/Day-1/variables.py
@@ -0,0 +1,9 @@
+'''
+variable is container used to data store 
+we  assign values using the "=" operator
+concatination 
+'''
+name="pavan"
+college="sietk"
+print(name+" "+college)
+print(f"{name} {college}")
