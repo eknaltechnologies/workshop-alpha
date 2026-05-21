9668e9e5b77841d12c1706f0736a8dff3bf27bff
dharani-thalluri <dharanirambabu36@gmail.com>
2026-05-21 23:23:00 +0530

    Added my day_1 and day_2 scripts

---
+++ dharani_thalluri/day_1/loops.py
@@ -0,0 +1,16 @@
+#loops
+for x in range(5):
+    print(x)
+#1
+count = 0
+while count < 10:
+    print(count)
+    count += 1
+
+#conditional statements     
+a = 200
+b = 33
+if b > a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a")                
