6ba2bc910eb2926f3961fb7674f03be9867717c6
Varun <thodupunoori.varun2006@gmail.com>
2026-05-22 13:45:18 +0530

    Added my Day-03 script

--- varun_kumar/Day-02/sets.py
+++ varun_kumar/Day-02/sets.py
@@ -1,87 +1,47 @@
-# example
-flowers = ["rose", "lily", "lotus", "jasmine"]
-print(flowers)
-# List length
-print(len(flowers))
-# data type of list
-print(type(flowers))
-# indexing
-print(flowers[0])
-print(flowers[1])
-print(flowers[2])
-print(flowers[3])
-# negative indexing
-print(flowers[-1])
-print(flowers[-2])
-print(flowers[-3])
-print(flowers[-4])
-# range of indexing
-print(flowers[1:4])
-print(flowers[0:3])
-print(flowers[:4])
-print(flowers[1:])
-#range of negative indexing
-print(flowers[-4:-1])
-print(flowers[-4:-3])
-#checking the presence of an element in the list
-print("rose" in flowers)
-#changing the value of an element in the list
-flowers[1] = ["sunflower"]
-#changing the value of an element in the list using negative indexing
-flowers[-1] = ["hibiscus"]
-#chaing the value of an element in the list using range of indexing
-flowers[0:2] = ["marigold", "daisy"]
-print(flowers)
-#using insert() method
-flowers.insert(2, "orchid")
-#using append() method
-flowers.append("tulip")
-print(flowers)
-#extend() method
-flowers.extend(["lotus", "jasmine"])
-print(flowers)
-#using remove() method
-flowers.remove("lotus")
-print(flowers)
-#using pop() method
-flowers.pop(3)
-print(flowers)
-#using del keyword
-del flowers[1]
-print(flowers)
-#using clear() method
-flowers.clear()
-print(flowers)
-#using for loop
-flowers = ["rose", "lily", "lotus", "jasmine"]
-for flower in flowers:
-    print(flower)
-# using while loop
-i = 0
-while i < len(flowers):
-    print(flowers[i])
-    i= i + 1    
-# using sort() method
-flowers.sort()
-print(flowers)
-# using sort descending order
-flowers.sort(reverse = True)
-print(flowers)
-# using case insensitive sort
-flowers.sort(key=str.lower)
-print(flowers)
-# using reverse() method
-flowers.reverse()
-print(flowers)
-#using copy() method
-flowers = flowers.copy()
-print(flowers)
-#using list() method
-flowers = list(flowers)
-print(flowers)
-#using slice operator
-flowers = flowers[:]
-print(flowers)
-# using sorted() method
-flowers = sorted(flowers)
-print(flowers)
+mylists={"apple","banana","cherry"}
+print(mylists)
+#add
+thisset = {"apple", "banana", "cherry"}
+
+thisset.add("orange")
+
+print(thisset)
+#remove
+thisset.remove("banana")
+print(thisset)
+#loop
+numbersset={23,24,25,26,27,28}
+for x in numbersset:
+    print(x)
+# join
+num1=(11,12,13,14,15)
+num2=(21,22,23,24,25)
+num3=num1+num2
+print(num3)
+#frozenset
+x = frozenset({"apple", "banana", "cherry"})
+print(x)
+print(type(x))
+#add
+students={"krithi","hema","deepu","harini"}
+students.add("minnu")
+print(students)
+#clear
+students.clear()
+print(students)
+#copy
+fruits = {"apple", "banana", "cherry"}
+
+x = fruits.copy()
+
+print(x)
+#difference
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+z = x.difference(y)
+
+print(z)
+#update
+
+x = {"apple", "banana", "cherry"}
---
+++ varun_kumar/Day-03/Restaurant.py
@@ -0,0 +1,119 @@
+import json
+
+# Load data from json file
+try:
+    with open("restaurant.json", "r") as f:
+        menu = json.load(f)
+
+except:
+    menu = {}
+
+while True:
+    print("\n---- RESTAURANT MENU ----")
+    print("1. Add Item")
+    print("2. View Menu")
+    print("3. Update Item")
+    print("4. Delete Item")
+    print("5. Order Food")
+    print("6. Exit")
+    choice = int(input("Enter choice: "))
+
+    # Add Item
+    if choice == 1:
+
+        item = input("Enter item name: ")
+        price = int(input("Enter item price: "))
+
+        menu[item] = price
+
+        print("Item Added Successfully")
+
+    # View Menu
+    elif choice == 2:
+
+        if menu == {}:
+            print("Menu is empty")
+
+        else:
+            print("\nAvailable Items")
+
+            for i, j in menu.items():
+                print(i, ":", j)
+
+    # Update Item
+    elif choice == 3:
+
+        item = input("Enter item name to update: ")
+
+        if item in menu:
+
+            new_price = int(input("Enter new price: "))
+
+            menu[item] = new_price
+
+            print("Item Updated")
+
+        else:
+            print("Item not found")
+
+    # Delete Item
+    elif choice == 4:
+
+        item = input("Enter item name to delete: ")
+
+        if item in menu:
+
+            del menu[item]
+
+            print("Item Deleted")
+
+        else:
+            print("Item not found")
+
+    # Order Food
+    elif choice == 5:
+
+        total = 0
+
+        while True:
+
+            print("\nMENU")
+
+            for i, j in menu.items():
+                print(i, ":", j)
+
+            food = input("Enter food name: ")
+
+            if food in menu:
+
+                qty = int(input("Enter quantity: "))
+
+                bill = menu[food] * qty
+
+                total += bill
+
+                print("Added Successfully")
+                print("Item Bill =", bill)
+
+            else:
+                print("Food not available")
+
+            more = input("Want more items? yes/no: ")
+
+            if more == "no":
+                break
+
+        print("\nTotal Bill =", total)
+
+    # Exit
+    elif choice == 6:
+
+        print("Thank You Visit Again")
+        break
+
+    else:
+        print("Invalid Choice")
+
+    # Save data
+    with open("restaurant.json", "w") as f:
+        json.dump(menu, f, indent=4)
