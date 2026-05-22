8af37746f8f8f2fffc1166ec4d097a55bd3674f1
shruthi <shruthigudalashruthi@gmail.com>
2026-05-22 21:30:39 +0530

    day-3 are topics files are updated

---
+++ day-3/dictionary.py
@@ -0,0 +1,6 @@
+# create a dictionary
+person ={
+    "name": "Alice",
+    "age": 30,
+    "city": "New york"
+}
---
+++ day-3/function.py
@@ -0,0 +1,6 @@
+#define function
+def greet(Name):
+    return "Hello",+Name+ "!"
+#call the fuction
+message = greet("buddy")
+print(message)
