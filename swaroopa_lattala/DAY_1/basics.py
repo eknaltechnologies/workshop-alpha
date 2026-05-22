f643c1d05357ceb04f7e8e447db7886116b467ac
L KRISHNA SWAROOPA <115877294+Swaroopalattala@users.noreply.github.com>
2026-05-22 22:15:46 +0530

    Added Day-1 basics file again

---
+++ swaroopa_lattala/DAY_1/basics.py
@@ -0,0 +1,13 @@
+#for loop
+teams=["RCB","LSG","CSK","GT"]
+for team in teams:
+    print("Leading teams:",team)
+#while loop
+count=3
+while count > 1:
+     print("HIGH")
+     count=count-1
+#
+n = int(input("enter any number:"))
+for i in range(n):
+        print(i*i)
