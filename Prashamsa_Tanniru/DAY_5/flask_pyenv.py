73474ee5755068d6c4efed3d99d7e6b659cd8e21
prashamsa6240 <prashamsatanniru@gmail.com>
2026-05-25 20:30:42 +0530

    Resolved Quality Error

--- Prashamsa_Tanniru/DAY_5/flask_pyenv.py
+++ Prashamsa_Tanniru/DAY_5/flask_pyenv.py
@@ -1,4 +1,4 @@
-from flask import Flask,request
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -64,4 +64,4 @@ def about(note_title):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) 
