fe47a6573165da1bce23be3d3a046fdb87c96584
L KRISHNA SWAROOPA <115877294+Swaroopalattala@users.noreply.github.com>
2026-05-26 23:58:36 +0530

    Fixed reordered impot to pass ruff linter

--- swaroopa_lattala/DAY_7/unit_converter_app.py
+++ swaroopa_lattala/DAY_7/unit_converter_app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for
 import json
+from flask import Flask, render_template, request, redirect, url_for
+
 
 app = Flask(__name__)
 def load_data():
