661c74496654224e45aa553187534fd1f34bbdba
L KRISHNA SWAROOPA <115877294+Swaroopalattala@users.noreply.github.com>
2026-05-27 14:29:44 +0530

    Formatted code using black

--- swaroopa_lattala/DAY_7/unit_converter_app.py
+++ swaroopa_lattala/DAY_7/unit_converter_app.py
@@ -3,6 +3,8 @@ import json
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
+
+
 def load_data():
     try:
         with open("unit_converter.json", "r") as f:
@@ -10,21 +12,23 @@ def load_data():
     except (FileNotFoundError, json.JSONDecodeError):
         return {"history": {}}
 
+
 def save_data(data):
     with open("unit_converter.json", "w") as f:
         json.dump(data, f, indent=4)
 
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     result = None
-    
+
     if request.method == "POST":
-        
+
         user_choice = int(request.form.get("choice"))
         val = float(request.form.get("value"))
-        
+
         converter = load_data()
-        
+
         if user_choice == 1:
             res = val * 1000
             key, value = f"{val} KM", f"{res} M"
@@ -53,13 +57,14 @@ def index():
             res = (val * 9 / 5) + 32
             key, value = f"{val} C", f"{res} F"
             converter["history"][key] = value
-            
+
         result = f"{key} = {value}"
-        
+
         converter["history"][key] = value
         save_data(converter)
-        
+
     return render_template("index.html", result=result)
+
 
 @app.route("/edit/<key>", methods=["GET", "POST"])
 def edit_record(key):
@@ -80,21 +85,24 @@ def edit_record(key):
         current_value=current_value,
     )
 
+
 @app.route("/history")
 def history():
     converter = load_data()
     return render_template("history.html", history=converter["history"])
 
+
 @app.route("/delete", methods=["POST"])
 def delete_record():
     key_to_delete = request.form.get("key")
     converter = load_data()
-    
+
     if key_to_delete in converter["history"]:
         del converter["history"][key_to_delete]
         save_data(converter)
-        
+
     return redirect(url_for("history"))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
